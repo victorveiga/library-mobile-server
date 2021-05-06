1ea10427aad17459275e8ee9877efaf324a5896e
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2021-05-05 23:05:09 -0300

    '.'

--- book/serializers.py
+++ book/serializers.py
@@ -11,9 +11,12 @@ class BookSerializer(ModelSerializer):
 
     # Custom fields functions #
     def get_status(self, instance):
+        return 1
         queryset        = Movement.objects.all()
-        queryset_load   = queryset.filter(status__iexact=1).filter(books__in=[instance.id])
-        queryset_return = queryset.filter(status__iexact=2).filter(books__in=[instance.id])
+        queryset_load   = queryset.filter(status__iexact=1)
+        queryset_load   = queryset_load.filter(books__in=[instance.id])
+        queryset_return = queryset.filter(status__iexact=2)
+        queryset_return = queryset_return.filter(books__in=[instance.id])
         
         if (len(queryset_load) > len(queryset_return)):
             return 2 # Not Available | Chacked out
